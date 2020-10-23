b680704d18760be03680c5dc57f71accb9c0f319
Bryan Vespignani <bryanvespignani@iMac-di-Bryan-2.lan>
2020-10-23 22:15:24 +0200

    Add m prefix for unit. Now variable names are refered to components. Limit decimal digits. Reactive components are now supported

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 import parser
 from sympy import solve
 
+cifre_mostrate = 7
+
 # Equazioni del circuito
 equ = parser.get_equation()
 
@@ -32,15 +34,53 @@ solutions = str(solve(equ, X)).split(',')
 # Sostituisco i valori nelle equazioni e ne calcolo il risultato
 # Infine stampa gli output
 for s in solutions:
+
+    # Elimino eventuali parentesi graffe derivanti dalla conversione
+    # Delle equazioni in stringhe
     if s[0] == "{":
         s = s[1:]
     if s[-1:] == "}":
         s = s[:-1]
+
+    # Rimpiazzo le variabili con i propri valori
     for val in variabili:
         s = s.replace(val, variabili[val])
+        # Per i componenti reattivi si ha s = jw
+        # Siccome in regime di corrente continua si ha 
+        # w = 2*π*f, dove f = 0 => w = s = 0
+        s = s.replace('s', '0')
+
+    # Elimino eventuali spazi dalla stringa
     s = s.strip()
+
+    # Separo il nome dell'incognita dal suo valore
+    #   res[0] -> nome dell'incognita
+    #   res[1] -> espressione algebrica con simboli
     res = s.split(":")
-    unit = ' V'
+
+    # Unità di misura
+    unit = ' '
+
+    # Calcolo il valore dell'incognita
+    value = float(str(eval(res[1])))
+
+    # Nel caso i valori siano piccoli, aggiungo milli
+    if value < 1:
+        value *= 1000;
+        unit += 'm'
+
+    # Imposto l'unità di misura in funzione della prima lettera
+    # del nome della variabile
     if res[0][0].lower() == 'i':
-        unit = ' A'
-    print(res[0] + " = " + str(eval(res[1])) + unit)
+        unit += 'A'
+    else:
+        unit += 'V'
+
+    if '_' in res[0]:
+        for val in variabili:
+            if variabili[val] in res[0]:
+                res[0] = res[0].replace(variabili[val], val)
+                break
+
+    # Sommo 1 alle cifre mostrate perche viene contato anche il .
+    print(res[0] + " = " + str(value)[:cifre_mostrate + 1] + unit)
